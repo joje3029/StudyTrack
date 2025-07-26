6c318bc4100bb49737639575a4253c74d74c17ad
joje3029 <joje3029@gmail.com>
2025-07-26 21:02:53 +0900

    [feat/db] PostgreSQL 도커 설정 및 백엔드 연결

--- studytrack-backend/build.gradle.kts
+++ studytrack-backend/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.postgresql:postgresql:42.7.3")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
